c2fdafd25ab1f9ec140cb00730c595bbdf9cbae9
Johnny Briones <jbrrii91@gmail.com>
2020-12-15 21:32:09 -0500

    transparent box in the middle of the app

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import Navigationbar from './components/NavigationBar'
-import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom'
 import Random from './Pages/Random'
 import Search from './Pages/Search'
+import TransparentBox from "./components/TransparentBox";
 
 function App() {
   return (
     <Router>
     <div className="App" >
       <Navigationbar />
-    </div>
+      <TransparentBox />
     <Switch>
     <Route path='/' component={Random} />
     <Route path='/search' component={Search} />
     </Switch>
+    </div>
     </Router>
   );
 }
---
+++ twitter/src/components/TransparentBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TransparentBox() {
+    return(
+        <div className ='background'>
+            <div className='transbox'>
+                <p>This is some text that is placed in the transparent box.</p>
+            </div>
+        </div>
+    )
+}
+
+export default TransparentBox
