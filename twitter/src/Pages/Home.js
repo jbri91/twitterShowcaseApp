0a94027117f51d4939ea9e205a585177e9c5f104
Johnny Briones <jbrrii91@gmail.com>
2020-12-16 23:39:34 -0500

    card added in TwitterCard

--- twitter/src/Pages/Home.js
+++ twitter/src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TransparentBox from './TransparentBox'
 
 
+
 function Home() {
     return(
         <div className='home'>
--- twitter/src/Pages/Random.js
+++ twitter/src/Pages/Random.js
@@ -1,8 +1,11 @@
 import React from "react";
+import TwitterCard from "../components/TwitterCard";
+
 
 function Random() {
   return (
     <div className="random">
+        <TwitterCard />
     </div>
   );
 }
