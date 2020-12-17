3f8d60692a436e486be2380f9e27db4b8d76aa00
Johnny Briones <jbrrii91@gmail.com>
2020-12-17 00:49:37 -0500

    twitterCard

--- twitter/src/components/TwitterCard.js
+++ twitter/src/components/TwitterCard.js
@@ -7,10 +7,16 @@ function TwitterCard() {
       <Card style={{ width: "18rem", margin: '35px' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Playstation</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+          Multiplayer maps & modes
+          <ul>
+          <li>Collision symbol Nuketown Holiday 24/7</li>
+          <li>Collision symbol Prop Hunt</li> 
+          <li>Collision symbol Much more</li>
+          Jump into the #BlackOpsColdWar Free Access Week, starting on December 17:
+          https://bit.ly/BOCWFreeTrial
+          </ul>
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
