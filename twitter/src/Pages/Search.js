7524323bb7331012411f46eb3a8266738766d4a8
Johnny Briones <jbrrii91@gmail.com>
2020-12-17 00:21:43 -0500

    userInput

--- twitter/src/Pages/Search.js
+++ twitter/src/Pages/Search.js
@@ -1,12 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import TwitterCard from "../components/TwitterCard";
 
 function Search() {
-    return(
-        <div className='search'>
-      <input type='text' placeholder='Search for User or Tweet'/> 
-        </div>  
-)
+  const cardStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    paddingTop: "150px",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
+  };
+  return (
+    <div className="search">
+      <input
+        style={{ justifyContent: "center", paddingTop: "50px" }}
+        type="text"
+        placeholder="Search for User or Tweet"
+      />
+      <div style={cardStyle}>
+        <TwitterCard />
+        <TwitterCard />
+        <TwitterCard />
+        <TwitterCard />
+        <TwitterCard />
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
